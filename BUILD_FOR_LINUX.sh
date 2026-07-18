c7225616746fde6210d442f0395c705504a62611
Diogo Regateiro <diogo.regateiro@gmail.com>
2026-07-18 10:52:51 +0100

    Update dependencies and migrate Linux build to build-package.js

--- BUILD_FOR_LINUX.sh
+++ BUILD_FOR_LINUX.sh
@@ -1,26 +1,13 @@
 cd "`dirname "$0"`"
 
-# Uses: https://github.com/electron-userland/electron-packager
-# To install it globally:
-#
-#     npm install electron-packager -g
-#
-
 # Clean
-rm -rf Inky-linux-x64/
+rm -rf AppImage
 rm -rf ReleaseUpload
 
-# Ensure it's correctly/fully installed first
-( cd app && npm install )
+# Install dependencies and build using build-package.js
+( cd app && npm install && npm run build-package -- -zip linux )
 
-# Linux
-electron-packager app Inky --platform=linux --arch=x64 --icon=resources/Icon.icns --extend-info=resources/info.plist --prune --asar.unpackDir="main-process/ink" --ignore="inklecate_mac"
-
-# Create a zip files ready for upload on Windows/Linux
-mkdir -p ReleaseUpload
-zip -r ReleaseUpload/Inky_linux.zip Inky-linux-x64
-
-#Prepare AppImage build structure
+# Prepare AppImage build structure
 mkdir -p AppImage/opt/inky
 mkdir -p AppImage/usr/share/pixmaps
 
@@ -30,5 +17,5 @@ cp resources/Icon1024.png AppImage/inky.png
 cp resources/Icon1024.png AppImage/usr/share/pixmaps/inky.png
 cp -r Inky-linux-x64/* AppImage/opt/inky/
 
-#Build AppImage File ready for upload
+# Build AppImage
 ARCH=x86_64 ./build/appimagetool-x86_64.AppImage -n AppImage ReleaseUpload/Inky.AppImage
